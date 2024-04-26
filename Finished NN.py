775a69c24087d6a2ba51717693456e7ca5062671
novazor <160160181+novazor@users.noreply.github.com>
2024-04-26 17:12:42 -0400

    Update Finished NN.py

--- Finished NN.py
+++ Finished NN.py
@@ -1,24 +1,24 @@
 import numpy as np
 
-#Sigmoid activation function
+# sigmoid activation function (chose this one over ReLU or tanh since this one is using binary classification)
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
     
 def sigmoid_derivative(x):
     return x * (1 - x)
 
-#Initializing network parameters
-input_layer_size = 2  # Number of features in the input data
-hidden_layer_size = 2  # Can be varied
-output_layer_size = 1  # For binary classification
+# initializing params
+input_layer_size = 2  
+hidden_layer_size = 2  
+output_layer_size = 1  
 
-np.random.seed(42)  # For reproducibility
+np.random.seed(42)  # Easier random num
 weights_input_hidden = np.random.uniform(size=(input_layer_size, hidden_layer_size))
 weights_hidden_output = np.random.uniform(size=(hidden_layer_size, output_layer_size))
 bias_hidden = np.random.uniform(size=(1, hidden_layer_size))
 bias_output = np.random.uniform(size=(1, output_layer_size))
 
-#Forward propagation function
+# forward propagation function
 def forward_propagate(inputs):
     hidden_layer_input = np.dot(inputs, weights_input_hidden) + bias_hidden
     hidden_layer_activation = sigmoid(hidden_layer_input)
@@ -26,32 +26,31 @@ def forward_propagate(inputs):
     predicted_output = sigmoid(output_layer_input)
     return predicted_output
 
-#Backpropagation function (Going back and adjusting weights based on the error
-#of the output compared to the expected result)
+# backpropagation function 
 def train(inputs, expected_output, learning_rate, iterations):
     global weights_input_hidden, weights_hidden_output, bias_hidden, bias_output
     for _ in range(iterations):
-        # Forward propagation
+        # forward propagation
         hidden_layer_input = np.dot(inputs, weights_input_hidden) + bias_hidden
         hidden_layer_activation = sigmoid(hidden_layer_input)
         output_layer_input = np.dot(hidden_layer_activation, weights_hidden_output) + bias_output
         predicted_output = sigmoid(output_layer_input)
         
-        # Calculating the error
+        # error calc
         error = expected_output - predicted_output
         d_predicted_output = error * sigmoid_derivative(predicted_output)
         
-        # Backpropagation
+        # backpropagation
         error_hidden_layer = d_predicted_output.dot(weights_hidden_output.T)
         d_hidden_layer = error_hidden_layer * sigmoid_derivative(hidden_layer_activation)
         
-        # Updating Weights and Biases
+        # updating weights/biases
         weights_hidden_output += hidden_layer_activation.T.dot(d_predicted_output) * learning_rate
         weights_input_hidden += inputs.T.dot(d_hidden_layer) * learning_rate
         bias_output += np.sum(d_predicted_output, axis=0, keepdims=True) * learning_rate
         bias_hidden += np.sum(d_hidden_layer, axis=0, keepdims=True) * learning_rate
 
-#Training the neural network
+# training
 inputs = np.array([[0,0], [0,1], [1,0], [1,1]])
 expected_output = np.array([[0], [1], [1], [0]])
 
@@ -60,5 +59,5 @@ iterations = 10000
 
 train(inputs, expected_output, learning_rate, iterations)
 
-#Test the network after training
+# testing the network
 print(forward_propagate(inputs))
